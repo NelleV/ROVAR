5f71f42bfdf3f6c57be99537d18496f3ba4af110
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-11-09 17:50:22 +0100

    Now can normalize images

--- TP3/src/utils.py
+++ TP3/src/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy import io
 
 negsamples = '../data/negsamples.mat'
@@ -17,3 +18,28 @@ def load_data():
     pos = io.matlab.mio.loadmat(possamples)
     neg = io.matlab.mio.loadmat(negsamples)
     return pos['possamples'], neg['negsamples']
+
+def normalise(images):
+    """
+    Normalises the images
+
+    Params
+    --------
+        images: array of images
+    """
+
+    mean = images.mean(axis=0).mean(axis=0)
+    cov = np.sqrt(
+        ((images - mean)** 2).sum(axis=0).sum(axis=0) / (images.shape[0] * \
+                                                         images.shape[1]))
+    norm_images = (images - mean) / cov
+    return norm_images
+
+
+if __name__ == "__main__":
+    pos, neg = load_data()
+    pos = normalise(pos)
+    neg = normalise(neg)
+    pos_reshaped = pos.reshape((pos.shape[0] * pos.shape[1], pos.shape[2]))
+    pos_reshaped = neg.reshape((neg.shape[0] * neg.shape[1], neg.shape[2]))
+
